4aa221f7fc826e3673ef0cbe4721a1bb223e8b82
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2023-07-23 18:07:54 +0200

    split NSG rules in separate files

---
+++ test02/nsg-rule-100.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_security_rule" "ssh-from-isp" {
+  resource_group_name         = azurerm_network_security_group.home.resource_group_name
+  network_security_group_name = azurerm_network_security_group.home.name
+  name                        = "ssh-from-isp"
+  description                 = <<-EOT
+    Allow SSH traffic only from ISP network.
+    Two ranges from the ISP have been provided.
+  EOT
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefixes     = ["94.227.0.0/16", "84.197.0.0/16"]
+  destination_address_prefix  = "*"
+}
---
+++ test02/nsg-rule-110.tf
@@ -0,0 +1,16 @@
+resource "azurerm_network_security_rule" "http-from-everywhere" {
+  resource_group_name         = azurerm_network_security_group.home.resource_group_name
+  network_security_group_name = azurerm_network_security_group.home.name
+  name                        = "http-from-everywhere"
+  priority                    = 110
+  direction                   = "Inbound"
+  description                 = <<-EOT
+    Allow HTTP and HTTPS traffic from everywhere.
+  EOT
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["80", "443"]
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
