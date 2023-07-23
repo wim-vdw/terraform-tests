7334787fb4c142d38147570fda33d5721a2a3f7b
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2023-07-23 11:24:14 +0200

    add virtual network

--- test02/main.tf
+++ test02/main.tf
@@ -1,4 +1,11 @@
 resource "azurerm_resource_group" "rg" {
   location = "northeurope"
-  name     = "rg-test02"
+  name     = "rg-${local.suffix}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vn-${local.suffix}-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["99.0.0.0/16"]
 }
