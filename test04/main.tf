075d0edf71087aa26ab222419391633ade7179fe
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2023-08-02 21:56:08 +0200

    add test04 with Azure dashboards

---
+++ test04/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "rg" {
+  location = "northeurope"
+  name     = "rg-test04"
+}
+
+resource "azurerm_portal_dashboard" "dashboard1" {
+  name                = "dashboard1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags = {
+    hidden-title = "dashboard1 long name"
+  }
+  dashboard_properties = <<DASH
+{
+  "lenses": {}
+}
+DASH
+}
+
+resource "azurerm_portal_dashboard" "dashboard2" {
+  name                = "dashboard2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags = {
+    hidden-title = "dashboard2 long name"
+  }
+  dashboard_properties = <<DASH
+{
+  "lenses": {}
+}
+
+DASH
+
+  lifecycle {
+    ignore_changes = [dashboard_properties]
+  }
+}
