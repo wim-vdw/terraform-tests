94ba501c6bb737dba2790e6f830acc896c4645e4
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2023-08-05 17:22:04 +0200

    implement dashboard template

--- test04/main.tf
+++ test04/main.tf
@@ -4,55 +4,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_portal_dashboard" "dashboard1" {
-  name                = "dashboard1"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                 = "dashboard1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  dashboard_properties = templatefile("templates/dashboard1.json", {})
   tags = {
     hidden-title = "Dashboard1 long name"
   }
-  dashboard_properties = <<DASH
-{
-  "lenses": {
-      "0": {
-        "order": 0,
-        "parts": {
-          "0": {
-            "position": {
-              "x": 0,
-              "y": 0,
-              "colSpan": 2,
-              "rowSpan": 2
-            },
-            "metadata": {
-              "inputs": [],
-              "type": "Extension/HubsExtension/PartType/ClockPart",
-              "settings": {
-                "content": {
-                  "timezoneId": "Romance Standard Time",
-                  "timeFormat": "HH:mm",
-                  "version": 1
-                }
-              }
-            }
-          }
-        }
-      }
-    },
-    "metadata": {
-      "model": {
-        "timeRange": {
-          "value": {
-            "relative": {
-              "duration": 24,
-              "timeUnit": 1
-            }
-          },
-          "type": "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
-        }
-      }
-    }
-}
-DASH
 }
 
 resource "azurerm_portal_dashboard" "dashboard2" {
@@ -66,7 +24,6 @@ resource "azurerm_portal_dashboard" "dashboard2" {
 {
   "lenses": {}
 }
-
 DASH
 
   lifecycle {
