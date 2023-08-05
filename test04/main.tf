8bb40e8c3a32a74278e090ee676806c10df8eaed
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2023-08-05 16:53:50 +0200

    update dashboard1

--- test04/main.tf
+++ test04/main.tf
@@ -12,7 +12,45 @@ resource "azurerm_portal_dashboard" "dashboard1" {
   }
   dashboard_properties = <<DASH
 {
-  "lenses": {}
+  "lenses": {
+      "0": {
+        "order": 0,
+        "parts": {
+          "0": {
+            "position": {
+              "x": 0,
+              "y": 0,
+              "colSpan": 2,
+              "rowSpan": 2
+            },
+            "metadata": {
+              "inputs": [],
+              "type": "Extension/HubsExtension/PartType/ClockPart",
+              "settings": {
+                "content": {
+                  "timezoneId": "Romance Standard Time",
+                  "timeFormat": "HH:mm",
+                  "version": 1
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "metadata": {
+      "model": {
+        "timeRange": {
+          "value": {
+            "relative": {
+              "duration": 24,
+              "timeUnit": 1
+            }
+          },
+          "type": "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
+        }
+      }
+    }
 }
 DASH
 }
