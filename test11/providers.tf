9a46820b0a357d91991736136518a9b86e497053
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-02-02 21:59:49 +0100

    corrections to kubernetes

--- test11/providers.tf
+++ test11/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0, <3.0.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
