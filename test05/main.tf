41ec4a05a2a61ca21e1f40a33bdfae97f9720ca7
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-04-07 13:03:11 +0200

    downgrade version of dashboard

--- test05/main.tf
+++ test05/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "dashboard" {
   source  = "app.terraform.io/wimvandenwyngaert/dashboard/azurerm"
-  version = ">= 4.0.0, < 5.0.0"
+  version = ">= 3.0.0, < 4.0.0"
 
   location       = azurerm_resource_group.rg.location
   resource_group = azurerm_resource_group.rg
