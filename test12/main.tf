3c52945e9feb167c2d95665c0dce2be046827c05
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-04-27 00:59:12 +0200

    update RGs

--- test12/main.tf
+++ test12/main.tf
@@ -1,6 +1,15 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   location = "northeurope"
-  name     = "rg-test12"
+  name     = "rg-test12-part1"
+  tags = {
+    author = "Wim Van den Wyngaert"
+    env    = "development"
+  }
+}
+
+resource "azurerm_resource_group" "rg2" {
+  location = "northeurope"
+  name     = "rg-test12-part2"
   tags = {
     author = "Wim Van den Wyngaert"
     env    = "development"
