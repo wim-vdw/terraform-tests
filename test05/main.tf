1c05b31832dc270ce70e9af2258e146ec9d864c9
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-10-19 20:29:50 +0200

    add random_password in remote state

--- test05/main.tf
+++ test05/main.tf
@@ -42,6 +42,16 @@ module "dashboard_alpha" {
   })
 }
 
+resource "random_password" "pwd" {
+  length           = 20
+  min_lower        = 1
+  min_upper        = 1
+  min_numeric      = 1
+  min_special      = 1
+  special          = true
+  override_special = "(){};&"
+}
+
 output "dashboard_id" {
   value = module.dashboard.id
 }
