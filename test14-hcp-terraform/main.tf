234ed83762abeffdd659dfd5c65ae0f65c044d9a
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-11-05 21:54:31 +0100

    fix deprecations

--- test14-hcp-terraform/main.tf
+++ test14-hcp-terraform/main.tf
@@ -15,11 +15,14 @@ resource "tfe_workspace" "terraform-tests-test05" {
   queue_all_runs    = false
   force_delete      = false
   working_directory = "test05-hcp-terraform"
-  execution_mode    = "remote"
-  agent_pool_id     = null
 
   vcs_repo {
     identifier                 = "wim-vdw/terraform-tests"
     github_app_installation_id = "ghain-2qdT7CovWHxFKtmE"
   }
+}
+
+resource "tfe_workspace_settings" "terraform-tests-test05-settings" {
+  workspace_id   = tfe_workspace.terraform-tests-test05.id
+  execution_mode = "remote"
 }
