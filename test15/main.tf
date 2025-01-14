8faf3becdbc2601c7dbf4f68efd69a87298dd292
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-01-14 16:38:49 +0100

    add AWS subnets

--- test15/main.tf
+++ test15/main.tf
@@ -15,3 +15,21 @@ resource "aws_vpc" "homelab2" {
     Name = "homelab2-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.homelab1.id
+  cidr_block = "10.255.0.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.homelab1.id
+  cidr_block = "10.255.1.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
