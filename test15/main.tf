92195bc1a817e4bf28bcc0113bb35165f8573773
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-01-15 21:05:12 +0100

    add routes and internet gateway

--- test15/main.tf
+++ test15/main.tf
@@ -1,23 +1,14 @@
-resource "aws_vpc" "homelab1" {
+resource "aws_vpc" "homelab" {
   cidr_block       = "10.255.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "homelab1-vpc"
-  }
-}
-
-resource "aws_vpc" "homelab2" {
-  cidr_block       = "10.254.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "homelab2-vpc"
+    Name = "homelab-vpc"
   }
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.homelab1.id
+  vpc_id     = aws_vpc.homelab.id
   cidr_block = "10.255.0.0/24"
 
   tags = {
@@ -26,10 +17,49 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.homelab1.id
+  vpc_id     = aws_vpc.homelab.id
   cidr_block = "10.255.1.0/24"
 
   tags = {
     Name = "private-subnet"
   }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.homelab.id
+
+  tags = {
+    Name = "main-ig"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.homelab.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.homelab.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
