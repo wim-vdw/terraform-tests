a76a680d8a4d33d3eb8bd50e50acc1a833ea1447
Wim Van den Wyngaert <57628659+wim-vdw@users.noreply.github.com>
2025-07-02 13:40:06 +0200

    Update main.tf

--- test05-hcp-terraform/main.tf
+++ test05-hcp-terraform/main.tf
@@ -25,23 +25,6 @@ module "dashboard" {
   })
 }
 
-module "dashboard_alpha" {
-  source  = "app.terraform.io/wimvandenwyngaert/dashboard/azurerm"
-  version = ">= 5.0.0, < 6.0.0"
-
-  location       = azurerm_resource_group.rg.location
-  resource_group = azurerm_resource_group.rg
-  name           = "sd-test-05-alpha"
-  title          = "Created via Terraform Cloud"
-  additional_tags = {
-    type = "Test dashboard"
-  }
-  dashboard_properties = templatefile("templates/dashboard_template.json", {
-    timezone = "Romance Standard Time",
-    format   = "HH:mm"
-  })
-}
-
 output "dashboard_id" {
   value = module.dashboard.id
 }
