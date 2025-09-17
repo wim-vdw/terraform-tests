53d8ba668ac50b93aa6f80542bcf6f8935468ca7
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-17 22:10:03 +0200

    add Github provider tests

---
+++ test16/locals.tf
@@ -0,0 +1,31 @@
+locals {
+  repos = {
+    argocd-setup = {
+      name          = "argocd-setup"
+      description   = "Argo CD installation and configuration"
+      visibility    = "public"
+      has_downloads = true
+      has_issues    = true
+      has_projects  = true
+      has_wiki      = true
+    },
+    argocd-apps = {
+      name          = "argocd-apps"
+      description   = "Argo CD application definitions"
+      visibility    = "public"
+      has_downloads = true
+      has_issues    = true
+      has_projects  = true
+      has_wiki      = true
+    },
+    argocd-k8s-resources = {
+      name          = "argocd-k8s-resources"
+      description   = "Kubernetes manifests or Helm charts used by Argo CD applications"
+      visibility    = "public"
+      has_downloads = true
+      has_issues    = true
+      has_projects  = true
+      has_wiki      = true
+    }
+  }
+}
