e841d1406bf279f64c32f891e173359db268b990
Wim Van den Wyngaert <57628659+wim-vdw@users.noreply.github.com>
2025-09-17 22:11:45 +0200

    add Github provider tests
    * upgrade AWS provider
    
    * add Github provider tests

---
+++ test16/main.tf
@@ -0,0 +1,10 @@
+resource "github_repository" "repo" {
+  for_each      = local.repos
+  name          = each.value.name
+  description   = each.value.description
+  visibility    = each.value.visibility
+  has_downloads = each.value.has_downloads
+  has_issues    = each.value.has_issues
+  has_projects  = each.value.has_projects
+  has_wiki      = each.value.has_wiki
+}
