9be9bfb17252808f162f3230efe2bee7278e9b51
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-18 09:53:32 +0200

    add main as default branch

--- test16/repository/main.tf
+++ test16/repository/main.tf
@@ -7,3 +7,13 @@ resource "github_repository" "repository" {
   has_projects  = var.has_projects
   has_wiki      = var.has_wiki
 }
+
+resource "github_branch" "main" {
+  repository = github_repository.repository.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "default" {
+  repository = github_repository.repository.name
+  branch     = github_branch.main.branch
+}
