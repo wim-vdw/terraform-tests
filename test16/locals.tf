46629c317e97ba7b94adf012625a1f171a33c5c1
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-18 20:26:34 +0200

    change title 1

--- test16/locals.tf
+++ test16/locals.tf
@@ -29,7 +29,7 @@ locals {
     },
     test = {
       name          = "test"
-      description   = "Test repository for Terraform GitHub provider"
+      description   = "Test repository for Terraform GitHub provider - 1"
       visibility    = "private"
       has_downloads = true
       has_issues    = true
