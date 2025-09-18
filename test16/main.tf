5bd3475aae110c25bb387f6a46df87ff584ac0e4
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-18 23:56:25 +0200

    read branch protection rules

--- test16/main.tf
+++ test16/main.tf
@@ -8,3 +8,13 @@ module "repository" {
   has_projects  = each.value.has_projects
   has_wiki      = each.value.has_wiki
 }
+
+
+data "github_repositories" "repos" {
+  query = "org:wim-vdw"
+}
+
+data "github_branch_protection_rules" "rules" {
+  count      = length(data.github_repositories.repos.names)
+  repository = data.github_repositories.repos.names[count.index]
+}
