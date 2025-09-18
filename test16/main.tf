07aa3e170be302bf074b8ef75f71b17fc9862648
Wim Van den Wyngaert <57628659+wim-vdw@users.noreply.github.com>
2025-09-18 23:58:39 +0200

    add branch protection rules (#181)
    * read branch protection rules
    
    * change branch protection rules

--- test16/main.tf
+++ test16/main.tf
@@ -8,3 +8,13 @@ module "repository" {
   has_projects  = each.value.has_projects
   has_wiki      = each.value.has_wiki
 }
+
+
+data "github_repositories" "repositories" {
+  query = "org:wim-vdw"
+}
+
+data "github_branch_protection_rules" "branch_protection_rules" {
+  count      = length(data.github_repositories.repositories.names)
+  repository = data.github_repositories.repositories.names[count.index]
+}
