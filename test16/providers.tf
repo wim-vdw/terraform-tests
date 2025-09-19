bb55929580639786513abc8d7313097c6b4219fa
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-19 23:23:16 +0200

    change local state file location

--- test16/providers.tf
+++ test16/providers.tf
@@ -1,4 +1,8 @@
 terraform {
+  backend "local" {
+    path = "/Users/wim/Documents/terraform-state/terraform.tfstate"
+  }
+
   required_providers {
     github = {
       source  = "integrations/github"
