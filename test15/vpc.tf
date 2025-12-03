392d6007081bcbaf6894fe263628e5c803e2558a
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-12-03 18:59:25 +0100

    refactor subnets

--- test15/vpc.tf
+++ test15/vpc.tf
@@ -15,21 +15,63 @@ resource "aws_default_route_table" "homelab" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.homelab.id
+  availability_zone = "eu-west-1a"
   cidr_block = "10.255.0.0/24"
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet1"
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.homelab.id
+  availability_zone = "eu-west-1b"
   cidr_block = "10.255.1.0/24"
 
   tags = {
-    Name = "private-subnet"
+    Name = "public-subnet2"
+  }
+}
+
+resource "aws_subnet" "public3" {
+  vpc_id     = aws_vpc.homelab.id
+  availability_zone = "eu-west-1c"
+  cidr_block = "10.255.2.0/24"
+
+  tags = {
+    Name = "public-subnet3"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.homelab.id
+  availability_zone = "eu-west-1a"
+  cidr_block        = "10.255.3.0/24"
+
+  tags = {
+    Name = "private-subnet1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.homelab.id
+  availability_zone = "eu-west-1b"
+  cidr_block        = "10.255.4.0/24"
+
+  tags = {
+    Name = "private-subnet2"
+  }
+}
+
+resource "aws_subnet" "private3" {
+  vpc_id            = aws_vpc.homelab.id
+  availability_zone = "eu-west-1c"
+  cidr_block        = "10.255.5.0/24"
+
+  tags = {
+    Name = "private-subnet3"
   }
 }
 
@@ -62,12 +104,31 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
   route_table_id = aws_route_table.private.id
 }
